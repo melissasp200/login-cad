730060fbbaa148e61baad01e25154670a1b643d7
Melissa <melissa.pinto@alunoescolassociais.org.br>
2024-07-04 09:00:40 -0300

    codigo js

---
+++ controller.js
@@ -0,0 +1,9 @@
+//Validar acesso em tela de login
+
+function acessar(){
+
+let loginEmail = document.getElementById('loginEmail').value;
+let loginSenha = document.getElementById('loginSenha').value;
+
+
+}
