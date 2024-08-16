ef5adee698768193968d8138df5d43c45cdf6a72
Melissa <melissa.pinto@alunoescolassociais.org.br>
2024-08-16 12:06:50 -0300

    add do codigo para a adicao do email

--- controller.js
+++ controller.js
@@ -20,29 +20,33 @@ if(!loginEmail || !loginSenha){
 //Função que armazena, em array, nome na tela de cadastro
 
 var dadosLista = [];
+var salvaEmail = [];
 
 function salvarUser(){
    let nomeUser = document.getElementById('nomeUser').value;
+   let email = document.getElementById('emailUser').value;
 
-   if(nomeUser){
-    dadosLista.push(nomeUser);
+   if(nomeUser && email){
+    dadosLista.push(nomeUser); 
+    salvaEmail.push(email);
     //console.log(dadosLista);
     crialista();
     document.getElementById('nomeUser').value = "";
-  
+    document.getElementById('emailUser').value = "";
    }else{
     alert("Favor informar o nome cadastrado");
 
-
    }
-}    
+  }
+
+    
 
 // Função para criar lista
 
 function crialista(){
- let tabela = document.getElementById('tabela').innerHTML = "<tr><th>nome Usuario</th><th>Açoes</th</tr>";
+ let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Usuário</th><th>Email</th><th>Ações</th></tr>";
 for(let i = 0; i <= (dadosLista.length-1); i++){
-   tabela += "<tr><td>"  +  dadosLista[i] + "</td><td> <button type= 'button' onclick= 'editar(this.parentNode.parentNode.rowIndex)'> Editar </button> <button type= 'button' onclick= 'excluir(this.parentNode.parentNode.rowIndex)'> Excluir </button></td></tr>";
+   tabela += "<tr><td>"  +  dadosLista[i] + "</td><td>" + salvaEmail[i] + "</td><td> <button type= 'button' onclick= 'editar(this.parentNode.parentNode.rowIndex)'> Editar </button> <button type= 'button' onclick= 'excluir(this.parentNode.parentNode.rowIndex)'> Excluir </button></td></tr>";
    document.getElementById('tabela').innerHTML = tabela;
 }
 }
@@ -50,8 +54,10 @@ for(let i = 0; i <= (dadosLista.length-1); i++){
 //Função para editar nomes da lista
 
  function editar(i){
-   document.getElementById('nomeUser').value = dadosLista [(i - i)];
+   document.getElementById('nomeUser').value = dadosLista[(i - i)];
    dadosLista.splice(dadosLista [(i - 1)], 1);
+   document.getElementById('emailUser').value = salvaEmail[(i - i)];
+   salvaEmail.splice(salvaEmail [(i - 1)], 1);
 
 
  }
@@ -61,4 +67,6 @@ for(let i = 0; i <= (dadosLista.length-1); i++){
   function excluir(i){
     dadosLista.splice((i-1), 1)
     document.getElementById('tabela').deleteRow(i);
+    
+  
   }
