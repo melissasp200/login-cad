2295453065c033a8676b0e3964ecdc08762d7d3d
Melissa <melissa.pinto@alunoescolassociais.org.br>
2024-08-29 10:17:47 -0300

    codigo modificado por causa do btn editar

--- controller.js
+++ controller.js
@@ -1,83 +1,85 @@
 //Validar acesso em tela de login
 
-function acessar(){
+function acessar() {
 
-let loginEmail = document.getElementById('loginEmail').value;
-let loginSenha = document.getElementById('loginSenha').value;
+  let loginEmail = document.getElementById('loginEmail').value;
+  let loginSenha = document.getElementById('loginSenha').value;
 
-if(!loginEmail || !loginSenha){
-  alert("Favor preencher todos os campos.");
+  if (!loginEmail || !loginSenha) {
+    alert("Favor preencher todos os campos.");
 
-}else{
-   //alert("Campos preenchidos com sucesso.");
-   window.location.href= 'cadastro.html';
+  } else {
+    //alert("Campos preenchidos com sucesso.");
+    window.location.href = 'cadastro.html';
 
-}
+  }
 
 }
 
 
 //Função que armazena, em array, nome e email na tela de cadastro
 
-var dadosLista = [];
-var salvaEmail = [];
-var salvaCPF = [];
+var cadUdser = [];
+//var dadosLista = [];
+//var salvaEmail = [];
+//var salvaCPF = [];
 
-function salvarUser(){
-   let nomeUser = document.getElementById('nomeUser').value;
-   let email = document.getElementById('emailUser').value;
-   let CPF = document.getElementById('CPFuser').value;
+function salvarUser() {
+  let nome = document.getElementById('nomeUser').value;
+  let email = document.getElementById('emailUser').value;
+  let cpf = document.getElementById('CPFuser').value;
+
+}
 
 
-   if(nomeUser && email && CPF){
-    dadosLista.push(nomeUser); 
-    salvaEmail.push(email);
-    salvaCPF.push(CPF);
+ if (nome && email && cpf) {
+    //dadosLista.push(nomeUser); 
+    //salvaEmail.push(email);
+    //salvaCPF.push(CPF);
+    cadUdser.push({ nome: nome, email: email, cpf: cpf });
 
-    
     //console.log(dadosLista);
     crialista();
     document.getElementById('nomeUser').value = "";
     document.getElementById('emailUser').value = "";
     document.getElementById('CPFuser').value = "";
 
-   }else{
-    alert("Favor informar o nome cadastrado");
+  } else {
+    alert("Favor informar o nome, email e CPF para cadastrado");
 
-   }
   }
 
-    
 
 // Função para criar lista
 
-function crialista(){
- let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Usuário</th><th>Email</th><th>CPF</th><th>Ações</th></tr>";
-for(let i = 0; i <= (dadosLista.length-1); i++){
-   tabela += "<tr><td>"  +  dadosLista[i] + "</td><td>" + salvaEmail[i] + "</td><td>" + salvaCPF[i] + "</td><td> <button type= 'button' onclick= 'editar(this.parentNode.parentNode.rowIndex)'> Editar </button> <button type= 'button' onclick= 'excluir(this.parentNode.parentNode.rowIndex)'> Excluir </button></td></tr>";
-   document.getElementById('tabela').innerHTML = tabela;
-}
+function crialista() {
+  let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Usuário</th><th>Email</th><th>CPF</th><th>Ações</th></tr>";
+  for (let i = 0; i <= (cadUdser.length - 1); i++) {
+    tabela += "<tr><td>" + cadUdser[i].nome + "</td><td>" + cadUdser[i].email + "</td><td>" + cadUdser[i].cpf + "</td><td> <button type= 'button' onclick= 'editar(this.parentNode.parentNode.rowIndex)'> Editar </button> <button type= 'button' onclick= 'excluir(this.parentNode.parentNode.rowIndex)'> Excluir </button></td></tr>";
+    document.getElementById('tabela').innerHTML = tabela;
+  }
 }
 
 //Função para editar nomes da lista
 
- function editar(i){
-   document.getElementById('nomeUser').value = dadosLista[(i - i)];
-   dadosLista.splice(dadosLista [(i - 1)], 1);
-   document.getElementById('emailUser').value = salvaEmail[(i - i)];
-   salvaEmail.splice(salvaEmail [(i - 1)], 1);
-   document.getElementById('CPFuser').value = salvaCPF[(i - i)];
-   salvaEmail.splice(salvaCPF [(i - 1)], 1);
+function editar(i) {
+  document.getElementById('nomeUser').value = cadUdser[(i - i)].nome;
+  document.getElementById('emailUser').value = cadUdser[(i - i)].email;
+  document.getElementById('CPFuser').value = cadUdser[(i - i)].cpf;
+  cadUdser.splice(cadUdser[(i - 1)], 1).nome;
+  cadUdser.splice(cadUdser[(i - 1)], 1).email;
+  cadUdser.splice(cadUdser[(i - 1)], 1).cpf;
 
 
 
- }
+}
 
- //Função para excluir
+//Função para excluir
 
-  function excluir(i){
-    dadosLista.splice((i-1), 1)
-    document.getElementById('tabela').deleteRow(i);
-    
-  
-  }
+function excluir(i) {
+  cadUdser.splice((i - 1), 1);
+  document.getElementById('tabela').deleteRow(i);
+
+
+}
+
